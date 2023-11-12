4f41d73549c4df2b8e53ab6cbf5ecdf4d2b57cd1
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2023-11-12 15:21:47 -0300

    Criando o modelo de domínio.

--- LanchesMac/Controllers/HomeController.cs
+++ LanchesMac/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using LanchesMac.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -32,12 +31,6 @@ namespace LanchesMac.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ LanchesMac/Models/Lanche.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LanchesMac.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [StringLength(100)]
+        public string DescricaoCurta { get; set; }
+
+        [StringLength(255)]
+        public string DescricaoDetalhada { get; set; }
+
+        public decimal Preco { get; set; }
+
+        [StringLength(200)]
+        public string ImagemUrl { get; set; }
+
+        [StringLength(200)]
+        public string ImagemThumbailUrl { get; set; }
+
+        public bool IsLanchePreferido { get; set; }
+
+        public bool EmEstoque { get; set; }
+
+        public int CategoriaId { get; set; }
+
+        public virtual Categoria Categoria { get; set; }
+    }
+}
